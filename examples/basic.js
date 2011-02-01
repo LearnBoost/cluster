b22da9aa8af9ecae35bcf3fecc9ea9c9c3435997
Tj Holowaychuk <tj@vision-media.ca>
2011-02-01 14:38:54 -0800

    Started Master

---
+++ examples/basic.js
@@ -0,0 +1,16 @@
+
+/**
+ * Module dependencies.
+ */
+
+var engine = require('../')
+  , http = require('http');
+
+var server = http.createServer(function(req, res){
+  var body = 'Hello World';
+  res.writeHead(200, { 'Content-Length': body.length });
+  res.end(body);
+});
+
+engine(server)
+  .listen(3000);
