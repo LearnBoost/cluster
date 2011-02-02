dcc4fcf1523b3796b0d5e71d6f823c9f05491bd9
Tj Holowaychuk <tj@vision-media.ca>
2011-02-02 12:43:43 -0800

    Added logger example

--- examples/logger.js
+++ examples/logger.js
@@ -7,10 +7,12 @@ var engine = require('../')
   , http = require('http');
 
 var server = http.createServer(function(req, res){
+  console.log('%s %s', req.method, req.url);
   var body = 'Hello World';
   res.writeHead(200, { 'Content-Length': body.length });
   res.end(body);
 });
 
 engine(server)
+  .use(engine.logger())
   .listen();
