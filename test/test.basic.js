58c73d0bd8bb538383c1129d78bd3dcaf046dbd8
Tj Holowaychuk <tj@vision-media.ca>
2011-02-02 13:43:35 -0800

    Added Master#{close,kill}()

---
+++ test/test.basic.js
@@ -0,0 +1,25 @@
+
+/**
+ * Module dependencies.
+ */
+
+var engine = require('../')
+  , assert = require('assert')
+  , http = require('http');
+
+var server = http.createServer(function(req, res){
+  res.writeHead(200);
+  res.end('Hello World');
+});
+
+engine = engine(server)
+  .listen(8888);
+
+http.get({ host: 'localhost', port: 3000 }, function(res){
+  res.on('data', function(chunk){
+    assert.equal('Hello World', chunk.toString());
+  });
+  res.on('end', function(){
+    engine.close();
+  });
+});
