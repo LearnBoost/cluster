c5d3abb7cdb1052c5c65909507adcbd4655c5972
Tj Holowaychuk <tj@vision-media.ca>
2011-02-02 14:54:57 -0800

    Started repl

---
+++ examples/repl.js
@@ -0,0 +1,19 @@
+
+/**
+ * Module dependencies.
+ */
+
+var engine = require('../')
+  , http = require('http');
+
+var server = http.createServer(function(req, res){
+  console.log('%s %s', req.method, req.url);
+  var body = 'Hello World';
+  res.writeHead(200, { 'Content-Length': body.length });
+  res.end(body);
+});
+
+engine(server)
+  .use(engine.stats())
+  .use(engine.repl('/var/run/engine'))
+  .listen();
