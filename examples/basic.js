7518cc67bbffa731c291435b36252b058e42e045
Tj Holowaychuk <tj@vision-media.ca>
2011-02-03 14:47:54 -0800

    removed logging from basic example

--- examples/basic.js
+++ examples/basic.js
@@ -7,7 +7,6 @@ var engine = require('../')
   , http = require('http');
 
 var server = http.createServer(function(req, res){
-  console.log('%s %s', req.method, req.url);
   var body = 'Hello World';
   res.writeHead(200, { 'Content-Length': body.length });
   res.end(body);
