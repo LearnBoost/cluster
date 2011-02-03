bc952c9c5fb83bdfa5094a4c9250ec7b0b51d6c8
Tj Holowaychuk <tj@vision-media.ca>
2011-02-03 09:32:04 -0800

    Added debug plugin

---
+++ examples/debug.js
@@ -0,0 +1,18 @@
+
+/**
+ * Module dependencies.
+ */
+
+var engine = require('../')
+  , http = require('http');
+
+var server = http.createServer(function(req, res){
+  console.log('%s %s', req.method, req.url);
+  var body = 'Hello World';
+  res.writeHead(200, { 'Content-Length': body.length });
+  res.end(body);
+});
+
+engine(server)
+  .use(engine.debug())
+  .listen();
