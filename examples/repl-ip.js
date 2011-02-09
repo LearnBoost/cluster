e54b830f5d64f78b4620d57c1df282a1f9c704e4
Tj Holowaychuk <tj@vision-media.ca>
2011-02-09 14:48:29 -0800

    Added ip repl example

---
+++ examples/repl-ip.js
@@ -0,0 +1,27 @@
+
+/**
+ * Module dependencies.
+ */
+
+var engine = require('../')
+  , http = require('http');
+
+var server = http.createServer(function(req, res){
+  console.log('%s %s', req.method, req.url);
+  var body = 'Hello World';
+  res.writeHead(200, { 'Content-Length': body.length });
+  res.end(body);
+});
+
+// custom repl function
+
+engine.repl.define('echo', function(master, sock, msg){
+  sock.write(msg + '\n');
+}, 'echo the given message');
+
+// $ telnet localhost 8888
+
+engine(server)
+  .use(engine.logger())
+  .use(engine.repl(8888))
+  .listen(3000);
