55d8a97c711fdae0a4b540db37713795c7e71b8f
Tj Holowaychuk <tj@vision-media.ca>
2011-02-10 10:59:40 -0800

    docs

--- examples/reload.js
+++ examples/reload.js
@@ -6,7 +6,7 @@
 var engine = require('../')
   , http = require('http');
 
-var body = 'Hello World'
+var body = 'Hello'
   , len = body.length;
 var server = http.createServer(function(req, res){
   res.writeHead(200, { 'Content-Length': len });
