5a94aa254060d927d409ea4fbc42a357b206a390
Tj Holowaychuk <tj@vision-media.ca>
2011-02-17 22:17:07 -0800

    Fixed Master#restart() signal support

--- examples/reload.js
+++ examples/reload.js
@@ -16,6 +16,8 @@ var server = http.createServer(function(req, res){
 });
 
 cluster(server)
+  // lower worker count will reload faster,
+  // however more will work just fine
   .set('workers', 1)
   .use(cluster.reload(__dirname))
   .use(cluster.debug())
