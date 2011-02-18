bb94fb0be46153ea0b5b3b5ff32d6ca295148320
Tj Holowaychuk <tj@vision-media.ca>
2011-02-18 13:50:59 -0800

    docs

--- examples/reload.js
+++ examples/reload.js
@@ -19,6 +19,9 @@ cluster(server)
   // lower worker count will reload faster,
   // however more will work just fine
   .set('workers', 1)
-  .use(cluster.reload(__dirname))
+  // be careful not to expose reload() to
+  // directories or files that cluster itself
+  // modifies, for example ./logs and ./pids
+  .use(cluster.reload('reload.js'))
   .use(cluster.debug())
   .listen(3000);
