d74c21b614cc246c05b522239d39ebe8e52137a6
Tj Holowaychuk <tj@vision-media.ca>
2011-02-17 22:04:13 -0800

    uncommented logger in debug example

--- examples/debug.js
+++ examples/debug.js
@@ -15,6 +15,6 @@ var server = http.createServer(function(req, res){
 
 cluster(server)
   .set('workers', 4)
-  //.use(cluster.logger('logs', 'debug'))
+  .use(cluster.logger('logs', 'debug'))
   .use(cluster.debug())
   .listen(3000);
