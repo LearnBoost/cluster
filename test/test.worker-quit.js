d4c88236b6a09cdec7d90b77b3d43c3dda6fd860
Tj Holowaychuk <tj@vision-media.ca>
2011-02-21 12:31:35 -0800

    Added worker SIGQUIT test

---
+++ test/test.worker-quit.js
@@ -0,0 +1,36 @@
+
+/**
+ * Module dependencies.
+ */
+
+var cluster = require('../')
+  , http = require('http');
+
+require('./common');
+
+var server = http.createServer(function(req, res){
+  res.writeHead(200);
+  res.end('Hello World');
+});
+
+cluster = cluster(server)
+  .set('workers', 1)
+  .listen(3000);
+
+cluster.on('listening', function(){
+  http.get({ host: 'localhost', port: 3000 }, function(res){
+    res.statusCode.should.equal(200);
+
+    // kill the worker
+    var pid = cluster.children[0].proc.pid;
+    process.kill(pid, 'SIGQUIT');
+  });
+});
+
+cluster.on('worker killed', function(worker){
+  worker.id.should.equal(0);
+  http.get({ host: 'localhost', port: 3000 }, function(res){
+    res.statusCode.should.equal(200);
+    cluster.close();
+  });
+});
