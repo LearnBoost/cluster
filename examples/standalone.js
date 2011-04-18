51b8a80449db4d63a40890a992ad1c8a440867ce
Tj Holowaychuk <tj@vision-media.ca>
2011-04-18 16:50:46 -0700

    refactored standalone example

--- examples/standalone.js
+++ examples/standalone.js
@@ -7,13 +7,12 @@ var cluster = require('../');
 
 var proc = cluster()
   .set('workers', 4)
+  .use(cluster.debug())
   .start();
 
 if (proc.isWorker) {
-  console.log('worker #%d started', process.env.CLUSTER_WORKER);
+  console.log('  worker #%d started', process.env.CLUSTER_WORKER);
   setInterval(function(){
-    console.log('processing job from worker #%d', process.env.CLUSTER_WORKER);
+    console.log('  processing job from worker #%d', process.env.CLUSTER_WORKER);
   }, 1000);
-} else {
-  console.log('master started');
 }
