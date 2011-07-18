c8daab82e4c46d07b7dbee4b7091426b63c03d10
Tj Holowaychuk <tj@vision-media.ca>
2011-07-18 14:42:16 -0700

    Added standalone restart test

---
+++ test/test.standalone-restart.js
@@ -0,0 +1,29 @@
+
+/**
+ * Module dependencies.
+ */
+
+var cluster = require('../');
+
+require('./common');
+
+cluster = cluster()
+  .set('workers', 2)
+  .start();
+
+if (cluster.isWorker) {
+  setTimeout(function(){
+    process.kill(process.env.CLUSTER_MASTER_PID, 'SIGUSR2');
+  }, 200);
+} else {
+  if (process.env.CLUSTER_REPLACEMENT_MASTER) {
+    var err;
+    try {
+      // make sure the previous parent was killed
+      process.kill(process.env.PARENT_PID, 0);
+    } catch (e) {
+      err = e;
+    }
+    if (!err) throw new Error('parent is running');
+  }
+}
